21094639466055ef80ecb3abfca46f1f3b794bb7
K Kollmann <git@k.kollmann.moe>
2015-11-22 18:10:42 +0100

    add arrays + array operations

--- MyiOSPlayground.playground/Contents.swift
+++ MyiOSPlayground.playground/Contents.swift
@@ -14,4 +14,28 @@ var age = 5
 // than concatenating strings with +
 "her name is " + name + " and she is " + String(age) + " years old"
 
+// --- ARRAYS ---
+var someNumbers = [1, 6, 0, 13]
+var someWords: [String] = ["these", "are", "all", "strings"]
+var mixedArray = ["this", 1, "is", "mixed"]
 
+// show the array type
+someNumbers.dynamicType
+someWords.dynamicType
+mixedArray.dynamicType
+
+// an array has to be declared empty before it can be filled
+var buildArray: [String] = []
+
+// adding elements
+buildArray.append("adding")
+buildArray.append("elements")
+buildArray.insert("some", atIndex: 1) // inserting at certain position
+buildArray += ["to", "this"] // appending another array
+buildArray.insertContentsOf(["more"], at: 2) // splicing another array
+
+// dumping output of an array
+dump(buildArray)
+
+// merging/combining arrays
+var combinedArray = someWords + buildArray
