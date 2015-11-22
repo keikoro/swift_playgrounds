0a5336efc912c4876a792119ad30644f1167e7e9
K Kollmann <git@k.kollmann.moe>
2015-11-22 19:05:47 +0100

    add more on arrays

--- MyiOSPlayground.playground/Contents.swift
+++ MyiOSPlayground.playground/Contents.swift
@@ -26,16 +26,26 @@ mixedArray.dynamicType
 
 // an array has to be declared empty before it can be filled
 var buildArray: [String] = []
+var initialiseArray = [String]()
 
-// adding elements
+// adding + removing + replacing elements
 buildArray.append("adding")
 buildArray.append("elements")
 buildArray.insert("some", atIndex: 1) // inserting at certain position
 buildArray += ["to", "this"] // appending another array
 buildArray.insertContentsOf(["more"], at: 2) // splicing another array
+buildArray.removeAtIndex(1)
+buildArray.removeLast()
+buildArray.removeLast()
+buildArray[2] = "items"
 
 // dumping output of an array
 dump(buildArray)
 
 // merging/combining arrays
 var combinedArray = someWords + buildArray
+
+// counting array elements
+buildArray.count
+buildArray.isEmpty
+
