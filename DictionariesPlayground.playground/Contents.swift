f6f3b05873ff422c8d98e830f8263a6dd3fed1f3
K Kollmann <git@k.kollmann.moe>
2015-11-22 19:30:47 +0100

    add dictionaries playground

---
+++ DictionariesPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+NSLocale.currentLocale().localeIdentifier
+NSLocale.availableLocaleIdentifiers()
+
+
+// --- DICTIONARIES ---
+var person = ["firstname": "Bob", "lastname": "Ross", "occupation": "painter"]
+person["firstname"]
